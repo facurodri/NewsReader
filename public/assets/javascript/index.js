02c51821fe17ad9d27beacf2b56361a9db3189f0
Facundo Rodriguez <facurodri91@gmail.com>
2019-06-06 18:38:14 -0400

    trying stuff, praying

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,3 +1,5 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+
 ("#scrape").on("click", function () {
     $.ajax({
         method: "GET",
@@ -6,5 +8,58 @@
         window.location = "/"
     });
 });
+//Save article btn
+$(".saveArticle").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "PUT",
+        url: "/article/save/" +thisId
+    }).done(function(data){
+        window.location ="/"
+    })
+});
+//Delete artcle button
+$(".delete-from-save").on("click", function(){
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/article/delete/"+thisId
+    }).done(function (data) {
+        window.location = "/saved"
+    })
+});
+
+$(".article-notes").on("click", function() {
+    $("#article-note-title").empty();
+    $("#article-note-id").empty();
+    $(".previou-notes").empty();
+
+    var thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method:"GET",
+        url:"/articles/"+thisId
+    })
+    .then(function(data) {
+        $("#article-note-title").append("<h3>" + data.title + "</h3>");
+        $("#article-note-title").attr("data-id" + thisId);
+        $("#article-note-id").append("<h5" + thisId + "</h5>");
+        $(".submit-noteBtn").attr("data-id",thisId);
+
+        if (data.note) {
+            for (var i = 0; data.note.length; i++){
+                var noteCard = $("<div class='card'>"); 
+                var noteCardBody = $("<div class=card-body>");
+                var noteCardBodyText = $("<p>").text(data.note[i].body);
+                $(".previous-notes").append(noteCard);
+                $(noteCard).append(noteCard);
+                $(noteCardBody).append(noteCardBodyText);
+                $(noteCardBodyText).append("<button class='btn btn-primary delete-note-btn' data-id=" +data.note[i]._id + ">X</button>");
+            }
+        };
+    });
+    $("#notesModal").modal("show");
+});
+
 
 
